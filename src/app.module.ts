c1847d0b19c3dc5471e775654c858566531bdcf6
Giovanna Cunha <giovannakelli@gmail.com>
2021-09-08 17:40:07 -0300

    typeorm integration was added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
